85ed109c4202376efa15060724d39a4ad02ba904
Joel Azemar <joel.azemar@gmail.com>
2025-12-07 17:19:57 +0100

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = Post.new(title: "A Post", body: "...", user_id: User.first.id)
   end
 
   # GET /posts/1/edit
